23382a08a45517bf548bb4607afdf0f9628b250f
Riya halbhavi <halbhaviriya@gmail.com>
2026-02-21 12:55:41 +0530

    Update department method in child classes

--- OOP/Override and Overload Methods/Override_Method.java
+++ OOP/Override and Overload Methods/Override_Method.java
@@ -1,30 +1,42 @@
-// -- Override -- //
+// -- Override Method -- //
 
+// Parent Class
 class College {
+  // Method to be overridden
   void department() {
     System.out.println("General Department");
   }
 }
 
+// Child Class 1
 class ComputerScience extends College {
-  @Override
+  // Overriding parent method
+  @Override 
   void department() {
-    System.out.println("General Department");
+    System.out.println("CS Department");
   }
 }
 
+// Child Class 2
 class ME extends College {
+  // Overriding parent method
   @Override
   void department() {
-    System.out.println("General Department");
+    System.out.println("ME Department");
   }
 }
 
+// MAIN Method Class
 public class Override_Method {
   public static void main(String[] args) {
+    // Reference of Parent class
     College c;
+
+    // Object of Child class 1
     c = new ComputerScience();
     c.department();
+
+    // Object of Child class 2
     c = new ME();
     c.department();
   }
